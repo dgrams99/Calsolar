5006100056179d96edd4963456b5e445c11be233
daniel <you@example.com>
2019-03-05 14:51:35 -0800

    backend for nodemailer

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default {
    
     send: function(data) {
-      return axios.post("http://localhost:3001/api/send", data);
+      return axios.post("/api/send", data);
     }
   };
